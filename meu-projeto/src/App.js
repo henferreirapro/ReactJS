4422e97425abb35bb5ae3c5408f22df848abd13d
Henrique Ferreira <62857389+henferreiraofc@users.noreply.github.com>
2022-08-12 11:18:06 -0300

    Basico de JSX
    1-Usamos a parte de fora apenas para importar e exportar.
    
    Import na parte de cima antes da 'function app', (linha 1).
    Export na parte de baixo depois da 'function app', (linha 24).
    
    2-Dentro da function app(), podemos criar nosss codigos como const (linha4, 5, 11) e function (linha7 a 9).
    
    Lembrando de sempre otimizar o nosso código para ficar o mais limpo possível.
    
    3-Sempre usamos a tag 'className' no reactJS, a tag 'class' é uma palavra reservada do react.
    
    OBS: sempre cheque o fechamentos das tags HTML pois o react irá reclamar caso haja erro.

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,11 +1,22 @@
-
 import './App.css';
 
 function App() {
+  const name = 'Henrique';
+  const newName = name.toUpperCase();
+
+  function sum(a, b) {
+    return a+b;
+  }
+
+  const url = 'https://via.placeholder.com/150';
+
   return (
     <div className="App">
-      <h1>Olá, Mundo!</h1>
-      
+      <h1>Alterando o JSX</h1>
+      <p>Olá {name}.</p>
+      <p>Seu nome em maiúsculo: {newName}.</p>
+      <p>A soma é: {sum(1, 2)}.</p>
+      <img src={url} alt="Minha imagem"></img>
     </div>
   );
 }
