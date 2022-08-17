d65f1d94780b0412f5b8c85b30cf624703ec9533
Henrique Ferreira <62857389+henferreiraofc@users.noreply.github.com>
2022-08-17 20:11:54 -0300

    useState
    1 - primeiro importamos o useState dentro de chaves no componente.
    Exemplo: import {useState} from 'react'
    
    2 - Para usarmos ele nós criamos uma const com conchetes e dentro add o nome do item que queremos e setamos o item que sera mudado.
    Exemplo: const [name, setName] = useState()
    
    3 - depois criamos o input com seus types, id, name, placeholder e por fim add um onChange para chamar o setName e assim alterar o item enviado pelo usuário.

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Evento from './components/Evento';
 import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <Evento numero="1" />
-      <Evento numero="2" />
       <Form />
     </div>
     
--- meu-projeto/src/components/Form.js
+++ meu-projeto/src/components/Form.js
@@ -1,15 +1,37 @@
+import {useState} from 'react'
+
 function Form() {
     function cadastroUsuario(e) {
         e.preventDefault()//usado para parar a execução do form e executar oq esta em seguida abaixo.
-        console.log("Usuário cadastrado com sucesso!")
+        console.log(`Usuário ${name} cadastrado com a senha ${password}`)
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
 
     return(
         <div>
             <h2>Meu cadastro:</h2>
             <form onSubmit={cadastroUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome" />
+                    <label mtmlFor="name">Nome:</label>
+                    <input
+                        type="text"
+                        id='name'
+                        name='name'
+                        placeholder="Digite seu nome"
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label mtmlFor="password">Senha:</label>
+                    <input
+                        type="password"
+                        id='password'
+                        name='password'
+                        placeholder='Digite a sua senha'
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar" />
